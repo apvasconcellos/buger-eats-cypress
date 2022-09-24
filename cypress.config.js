2a00b6d5a4224f69cb5edd6c5e436e991b81017e
apvasconcellos <avasconcellospereira@hotmail.com>
2022-09-23 22:36:48 -0300

    cypress-dash

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+  projectId: "7woo4k",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }, 
     baseUrl:"https://buger-eats-qa.vercel.app"
   }, 
-  viewportWidth: 1440, viewportHeight: 900, 
+  viewportWidth: 1440, viewportHeight: 900 
 });
